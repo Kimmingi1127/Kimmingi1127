bafc937b110ce78f609d96a937df20ac105ef14c
MinGi <zxc12250@naver.com>
2025-10-15 11:08:21 +0900

    react_Hook

---
+++ study/1015/react_hook/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+// import "@fontsource/material-icons";
+import ReactHookPage from './pages/ReactHookPage';
+
+function App() {
+  return (
+    <ReactHookPage />
+  );
+}
+
+export default App;
