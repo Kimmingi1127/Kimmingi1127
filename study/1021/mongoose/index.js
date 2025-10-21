0068d9bf4d9af7d711c0eb3c58330d5da679a946
MinGi <zxc12250@naver.com>
2025-10-21 12:49:51 +0900

    mongoose

--- study/1021/mongoose/index.js
+++ study/1021/mongoose/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import postRoutes from "./routes/postRoutes.js";
+import postRoutes from "./routes/post.routes.js";
 
 dotenv.config();
 const app = express();
