6864dc4db8dc8fe0cd7fc33fba2beeeb9c4ac52a
MinGi <zxc12250@naver.com>
2025-10-22 11:34:58 +0900

    board

--- study/1022/borad/app.js
+++ study/1022/borad/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const postService = require('./services/post-service');
 const app = express();
 
-app.engine("handlebars", handlebars.engine());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const mongodb = require('./configs/mongodb-connection');
+const mongodbConnection = require('./configs/mongodb-connection');
+
+// app.engine("handlebars", handlebars.engine());
+app.engine(
+    "handlebars", handlebars.create({
+        helpers: require("./configs/handlebars-helper"),
+    }).engine
+);
 app.set("view engine", "handlebars");
 app.set("views", __dirname + "/views");
 
@@ -14,4 +26,21 @@ app.get("/write", (req, res) => {
   res.render("write", { title: "테스트 게시판" });
 });
 
-app.listen(3000);
+app.post("/write", async (req, res) => {
+  const post = req.body;
+  const result = await postService.writePost(collection, post);
+  res.redirect(`/detail/${result.insertedId}`)
+});
+
+app.get("/detail/:id", (req, res) => {
+  res.render("detail", { title: "테스트 게시판" });
+});
+
+let collection;
+
+app.listen(3000, async () => {
+    console.log("Server started!");
+    const mongoClient = await mongodbConnection();
+    collection = mongoClient.db().collection("post");
+    console.log("MongoDB connected");
+});
