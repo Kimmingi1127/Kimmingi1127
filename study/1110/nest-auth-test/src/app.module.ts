2eaa6e94978de2bc57522d3586d3118fbe4c9b09
Mingi Kim <zxc12250@naver.com>
2025-11-10 09:52:28 +0900

    login, sqlite

--- study/1110/nest-auth-test/src/app.module.ts
+++ study/1110/nest-auth-test/src/app.module.ts
@@ -3,16 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    UserModule,
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: 'db.sqlite',
+      database: 'nest-auth-test.sqlite',
+      entities: [User],
       synchronize: true,
       logging: true,
     }),
+    UserModule,
+    AuthModule,
+    ConfigModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
